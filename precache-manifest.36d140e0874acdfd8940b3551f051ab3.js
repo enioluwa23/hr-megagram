0d84723b2f3acf3e98999d109faeb99a47a13411
Enioluwa Segun <eos24@drexel.edu>
2019-08-20 13:38:40 -0400

    Updates

---
+++ precache-manifest.36d140e0874acdfd8940b3551f051ab3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e7e826b7657a350bcbe6f5c27c7323",
+    "url": "/hr-megagram/index.html"
+  },
+  {
+    "revision": "d445d25450882a2b680e",
+    "url": "/hr-megagram/static/css/main.9056f04b.chunk.css"
+  },
+  {
+    "revision": "579980ca8f387548ff7c",
+    "url": "/hr-megagram/static/js/2.738e2d7b.chunk.js"
+  },
+  {
+    "revision": "d445d25450882a2b680e",
+    "url": "/hr-megagram/static/js/main.3c277487.chunk.js"
+  },
+  {
+    "revision": "3fc64aacf09b431ed70e",
+    "url": "/hr-megagram/static/js/runtime~main.2bfd97de.js"
+  },
+  {
+    "revision": "a58cef4a66300ecdff4bc9c96cccd8bb",
+    "url": "/hr-megagram/static/media/backdrop.a58cef4a.jpg"
+  }
+]);
